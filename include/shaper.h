f76b98f9f1aa8b7de514525f843023e69cf67984
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-08 23:57:37 +0530

    Add shaper.h header file for buffer management

---
+++ include/shaper.h
@@ -0,0 +1,10 @@
+#ifndef SHAPER_H
+#define SHAPER_H
+
+#include <stdint.h>
+
+// தமிழ் ஷேப்பிங் ஸ்டேட்ஸ் மற்றும் பஃபர் மேனேஜ்மென்ட்
+void shaper_reset_buffer(void);
+const char* shaper_process_key(uint32_T unicode_char);
+
+#endif // SHAPER_H
