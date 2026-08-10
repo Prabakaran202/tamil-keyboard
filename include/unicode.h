ab9efe0fd3072eaf5c4ea6845d0156d9e358692f
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-10 16:26:52 +0530

    Add unicode.h header for Tamizhi character printing

---
+++ include/unicode.h
@@ -0,0 +1,7 @@
+#ifndef TAMIZHI_UNICODE_H
+#define TAMIZHI_UNICODE_H
+
+// 1-byte Tamizhi கோடை வாங்கி UTF-8 ஆக ஸ்கிரீனில் பிரிண்ட் செய்யும் ஃபங்ஷன்
+void print_tz_char(unsigned char tz_code);
+
+#endif // TAMIZHI_UNICODE_H
