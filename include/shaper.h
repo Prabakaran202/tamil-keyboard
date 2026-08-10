a5db20b80e0332d3883c58ace6debe18b3e8cc13
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-10 16:57:21 +0530

    Update shaper.h

--- include/shaper.h
+++ include/shaper.h
@@ -5,6 +5,6 @@
 
 // தமிழ் ஷேப்பிங் ஸ்டேட்ஸ் மற்றும் பஃபர் மேனேஜ்மென்ட்
 void shaper_reset_buffer(void);
-const char* shaper_process_key(uint32_T unicode_char);
+const char* shaper_process_key(uint32_t unicode_char);
 
 #endif // SHAPER_H
