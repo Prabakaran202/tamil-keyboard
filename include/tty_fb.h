0b3106f04b199e7cd95e959e5fd8dbb4079ee1b8
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-08-11 22:56:00 +0530

    Update tty_fb.h

--- include/tty_fb.h
+++ include/tty_fb.h
@@ -1,1 +1,17 @@
 
+#ifndef TTY_FB_H
+#define TTY_FB_H
+
+// Framebuffer-ஐ துவக்குவதற்கான ஃபங்ஷன்
+int fb_init(void);
+
+// ஒரு குறிப்பிட்ட இடத்தில் 30x30 பாக்ஸை (Bounding Box) வரைவதற்கான ஃபங்ஷன்
+void fb_draw_box(int start_x, int start_y, int width, int height);
+
+// மெமரியை க்ளீன் செய்து Framebuffer-ஐ மூடுவதற்கான ஃபங்ஷன்
+void fb_cleanup(void);
+
+// தற்காலிகமாக வேலை செய்கிறதா என சோதிக்க ஒரு டெஸ்ட் ஃபங்ஷன்
+int fb_run_test(void);
+
+#endif // TTY_FB_H
